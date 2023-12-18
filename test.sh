d675546609941cf1a534f77e0b1c108837041c9d
Siddhant <s1d.official011001011@gmail.com>
2023-12-18 09:05:06 +0000

    asdasd

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Path to the image file
+image_path="sample.png"
+
+# Number of times to send the same image
+iterations=20  # Change this value to the desired number of iterations
+
+# API endpoint
+endpoint="https://mosaic-api.gokapturehub.com/process-my-image"
+
+# Loop to send the same image multiple times
+for ((i = 1; i <= iterations; i++)); do
+  echo "Sending image iteration $i"
+  curl -X POST -F "image=@$image_path" "$endpoint"
+done
